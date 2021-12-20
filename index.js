844d0cfe5bc1161751da88db22ea472fe3bcd17f
Addminus <adrianweniger@gmail.com>
2021-12-20 16:11:04 +0100

    update package version; add necessray hooks

--- index.js
+++ index.js
@@ -1,52 +1,68 @@
-import sdk from '@lifinance/sdk'
-import ethers from 'ethers'
+import sdk from "@lifinance/sdk";
+import ethers from "ethers";
 
-const Lifi = sdk.default
+const Lifi = sdk.default;
 
 async function demo() {
   // setup wallet
   if (!process.env.MNEMONIC) {
-    console.warn('Please specify a MNEMONIC phrase in your environment variables: `export MNEMONIC="..."`')
-    return
+    console.warn(
+      'Please specify a MNEMONIC phrase in your environment variables: `export MNEMONIC="..."`'
+    );
+    return;
   }
-  console.log('>> Setup Wallet')
-  const provider = new ethers.providers.JsonRpcProvider('https://polygon-rpc.com/', 137)
-  const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC).connect(provider)
-
+  console.log(">> Setup Wallet");
+  const provider = new ethers.providers.JsonRpcProvider(
+    "https://polygon-rpc.com/",
+    137
+  );
+  const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC).connect(
+    provider
+  );
 
   // get Route
-  console.log('>> Request route')
+  console.log(">> Request route");
   const routeRequest = {
     fromChainId: 137, // Polygon
-    fromAmount: '1000000', // 1 USDT
-    fromTokenAddress: '0xc2132d05d31c914a87c6611c10748aeb04b58e8f', // USDT
+    fromAmount: "1000000", // 1 USDT
+    fromTokenAddress: "0xc2132d05d31c914a87c6611c10748aeb04b58e8f", // USDT
     toChainId: 100, // xDai
-    toTokenAddress: '0x4ecaba5870353805a9f068101a40e0f32ed605c6', // USDT
+    toTokenAddress: "0x4ecaba5870353805a9f068101a40e0f32ed605c6", // USDT
     options: { slippage: 0.03 },
-  }
+  };
 
-  const routeResponse = await Lifi.getRoutes(routeRequest)
-  const route = routeResponse.routes[0]
-  console.log('>> Got Route')
-  console.log(route)
-
+  const routeResponse = await Lifi.getRoutes(routeRequest);
+  const route = routeResponse.routes[0];
+  console.log(">> Got Route");
+  console.log(route);
 
   // execute Route
-  console.log('>> Start Execution')
+  console.log(">> Start Execution");
   const settings = {
     updateCallback: (updatedRoute) => {
-      let lastExecution
+      let lastExecution;
       for (const step of updatedRoute.steps) {
         if (step.execution) {
-          lastExecution = step.execution
+          lastExecution = step.execution;
         }
       }
-      console.log(lastExecution)
-    }
-  }
-  await Lifi.executeRoute(wallet, route, settings)
+      console.log(lastExecution);
+    },
+    switchChainHook: async (requiredChainId) => {
+      console.log(">>Switching Chains");
+      const provider = new providers.JsonRpcProvider(
+        "https://rpc.xdaichain.com/",
+        requiredChainId
+      );
+      const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC).connect(
+        provider
+      );
+      return wallet;
+    },
+  };
+  await Lifi.executeRoute(wallet, route, settings);
 
-  console.log('DONE')
+  console.log("DONE");
 }
 
-demo()
+demo();
--- index.ts
+++ index.ts
@@ -1,52 +1,72 @@
-import Lifi, { ChainId, CoinKey, Execution, ExecutionSettings, findDefaultToken } from '@lifinance/sdk'
-import { providers, Wallet } from 'ethers'
+import Lifi, {
+  ChainId,
+  CoinKey,
+  Execution,
+  ExecutionSettings,
+  findDefaultToken,
+} from "@lifinance/sdk";
+import { providers, Wallet } from "ethers";
+
+const mnemonic = process.env.MNEMONIC || "";
 
 async function demo() {
   // setup wallet
   if (!process.env.MNEMONIC) {
-    console.warn('Please specify a MNEMONIC phrase in your environment variables: `export MNEMONIC="..."`')
-    return
+    console.warn(
+      'Please specify a MNEMONIC phrase in your environment variables: `export MNEMONIC="..."`'
+    );
+    return;
   }
-  console.log('>> Setup Wallet')
-  const provider = new providers.JsonRpcProvider('https://polygon-rpc.com/', 137)
-  const wallet = Wallet.fromMnemonic(process.env.MNEMONIC).connect(provider)
-
+  console.log(">> Setup Wallet");
+  const provider = new providers.JsonRpcProvider(
+    "https://polygon-rpc.com/",
+    137
+  );
+  const wallet = Wallet.fromMnemonic(mnemonic).connect(provider);
 
   // get Route
-  console.log('>> Request route')
+  console.log(">> Request route");
   const routeRequest = {
     fromChainId: ChainId.POL, // Polygon
-    fromAmount: '1000000', // 1 USDT
+    fromAmount: "1000000", // 1 USDT
     fromTokenAddress: findDefaultToken(CoinKey.USDT, ChainId.POL).address,
     toChainId: ChainId.DAI, // xDai
     toTokenAddress: findDefaultToken(CoinKey.USDT, ChainId.DAI).address,
-    options: { 
+    options: {
       slippage: 0.03, // = 3%
       allowSwitchChain: false, // execute all transaction on starting chain
     },
-  }
-  const routeResponse = await Lifi.getRoutes(routeRequest)
-  const route = routeResponse.routes[0]
-  console.log('>> Got Route')
-  console.log(route)
-
+  };
+  const routeResponse = await Lifi.getRoutes(routeRequest);
+  const route = routeResponse.routes[0];
+  console.log(">> Got Route");
+  console.log(route);
 
   // execute Route
-  console.log('>> Start Execution')
+  console.log(">> Start Execution");
   const settings: ExecutionSettings = {
     updateCallback: (updatedRoute) => {
-      let lastExecution: Execution | undefined = undefined
+      let lastExecution: Execution | undefined = undefined;
       for (const step of updatedRoute.steps) {
         if (step.execution) {
-          lastExecution = step.execution
+          lastExecution = step.execution;
         }
       }
-      console.log(lastExecution)
-    }
-  }
-  await Lifi.executeRoute(wallet, route, settings)
+      console.log(lastExecution);
+    },
+    switchChainHook: async (requiredChainId: number) => {
+      console.log(">>Switching Chains");
+      const provider = new providers.JsonRpcProvider(
+        "https://rpc.xdaichain.com/",
+        requiredChainId
+      );
+      const wallet = Wallet.fromMnemonic(mnemonic).connect(provider);
+      return wallet;
+    },
+  };
+  await Lifi.executeRoute(wallet, route, settings);
 
-  console.log('>> Done')
+  console.log(">> Done");
 }
 
-demo()
+demo();
