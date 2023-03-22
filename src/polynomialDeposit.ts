001954c6010b63646e4361fe621a337914221015
Max Klenk <max@klenk.biz>
2023-03-22 01:07:03 +0100

    clean polynomial call

--- src/polynomialDeposit.ts
+++ src/polynomialDeposit.ts
@@ -6,11 +6,13 @@ const getPolynomialQuote = async (fromChain: ChainId, fromToken: string, userAdd
   const sETH_OPT = '0xE405de8F52ba7559f9df3C368500B6E6ae6Cee49'
   const POLYNOMIAL_ETHEREUM_CONTRACT_OPT = '0x2D46292cbB3C601c6e2c74C32df3A4FCe99b59C7'
   const POLYNOMIAL_ABI = ['function initiateDeposit(address user, uint amount) external']
+  const POLYNOMIAL_GAS_LIMIT = '200000'
 
   // contract call
-  const stakeTx = await new ethers.Contract(
+  const contract = new ethers.Contract(
     POLYNOMIAL_ETHEREUM_CONTRACT_OPT, POLYNOMIAL_ABI
-  ).populateTransaction.initiateDeposit(userAddress, amount)
+  )
+  const stakeTx = await contract.populateTransaction.initiateDeposit(userAddress, amount)
 
   // quote
   const quoteRequest: ContractCallQuoteRequest = {
@@ -22,7 +24,7 @@ const getPolynomialQuote = async (fromChain: ChainId, fromToken: string, userAdd
     toAmount: amount,
     toContractAddress: stakeTx.to!,
     toContractCallData: stakeTx.data!,
-    toContractGasLimit: '200000',
+    toContractGasLimit: POLYNOMIAL_GAS_LIMIT,
   }
 
   return getLifi().getContractCallQuote(quoteRequest)
@@ -31,13 +33,13 @@ const getPolynomialQuote = async (fromChain: ChainId, fromToken: string, userAdd
 const run = async () => {
   console.log('Polynomial Demo: Deposit sETH on Optimism')
 
+  // config
   const fromChain = ChainId.ETH
   const fromToken = ethers.constants.AddressZero
-
-  const signer = await getSigner(fromChain)
   const amount = ethers.utils.parseEther('0.04').toString()
 
   try {
+    const signer = await getSigner(fromChain)
     // get quote
     const quote = await getPolynomialQuote(fromChain, fromToken, await signer.getAddress(), amount)
     console.log('Quote', quote)
